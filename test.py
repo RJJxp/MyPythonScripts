b3694474382f8b939439c4c2d1d36cbe3b3d8457
rjp <rjjjjjp1997@163.com>
2020-04-09 09:34:42 +0800

    update

--- test.py
+++ test.py
@@ -2,10 +2,10 @@ import os
 import itertools
 import time
 from PIL import Image as PilImage
-import imageio
+
 
 print('1' == 1)
-# s = "rjp_0001.jpg"
+s = "rjp_0001.jpg"
 # folder_1 = "/home/rjp/Documents"
 # folder_2 = "/home/rjp/Documents/test1"
 # folder_3 = "/home/rjp/Documents/test1/test2"
@@ -13,15 +13,16 @@ print('1' == 1)
 # s1 = [1, 2, 3, 5, 6]
 # s2 = [6, 6, 1, 4, 5]
 # s3 = [9, 1, 4, 5, 3]
+print (s[-8:-4])
 
 # ss = s3[-3:-1]
 # print(ss)
-try:
-    PilImage.open('/home/rjp/Desktop/420281199411046539_0002.bmp')
-except IOError:
-    print('There is an Error')
-else:
-    print('no problem')
+# try:
+#     PilImage.open('/home/rjp/Desktop/420281199411046539_0002.bmp')
+# except IOError:
+#     print('There is an Error')
+# else:
+#     print('no problem')
 # imageio.imread('/home/rjp/Desktop/420281199411046539_0002.bmp')
 # os.mkdir(folder_2)
 # os.mkdir(folder_3)
