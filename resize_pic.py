c71c77fa75a35a957d868e0980aa6c52f458f13b
Ren Jiaping <rjjjjjp1997@outlook.com>
2020-08-05 13:01:29 +0800

    update resize_pic.py

--- resize_pic.py
+++ resize_pic.py
@@ -21,14 +21,18 @@ def resizeIt(in_dir, out_dir, resize_w):
         print('outputdir already exists.')
     os.mkdir(out_dir)
     
+    count = 0
     for pic_name in os.listdir(in_dir):
         pic_path = os.path.join(in_dir, pic_name)
         pic = PilImage.open(pic_path)
         pic_w, pic_h = pic.size
         resize_h = resize_w / pic_w * pic_h
         pic_resize = pic.resize((resize_w, int(resize_h)))
-        save_path = os.path.join(out_dir, pic_name)
-        pic_resize.save(save_path)
+        pic_rgb = pic_resize.convert("RGB")
+        pic_save_name = "%04d" %count + ".jpg"
+        save_path = os.path.join(out_dir, pic_save_name)
+        pic_rgb.save(save_path)
+        count = count + 1
         print("resized %s" %pic_name)
 
     print("finished all pics in in_dir resizing.")
